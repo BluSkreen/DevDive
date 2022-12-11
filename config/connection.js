cb790c9fc3261b6bb09d2e23f9303fc0a58b25e5
BluSkreen <jasonduran303@gmail.com>
2022-12-11 13:40:58 -0700

    templates for html, used material template in 'main.handlebars'. Going to test the homeroute and page template on heroku. Also, changed homeroute to only display homepage(homepage is blank) so that we can see main

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,13 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(
-    process.env.JAWS_NAME,
-    process.env.JAWS_USER,
-    process.env.JAWS_PASSWORD,
-    {
-      host: process.env.JAWSDB_URL,
-      dialect: "mysql",
-      port: DB_PORT
-    }
-  );
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: "localhost",
       dialect: "mysql",
-      port: DB_PORT
-    }
-  );
-}
+      port: 3306,
+    });
 
 module.exports = sequelize;
