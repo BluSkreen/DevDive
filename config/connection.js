82345e840ab2542fcb200bcba0ecd88e88dcc37c
BluSkreen <jasonduran303@gmail.com>
2022-12-11 14:06:27 -0700

    jaws

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,16 @@ require("dotenv").config();
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+  ? new Sequelize(
+    process.env.JAWS_NAME,
+    process.env.JAWS_USER,
+    process.env.JAWS_PASSWORD,
+    {
+      host: process.env.JAWSDB_URL,
+      dialect: 'mysql',
+      port: 3306
+    }
+  )
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: "localhost",
       dialect: "mysql",
