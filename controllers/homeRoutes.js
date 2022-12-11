f024b46c3633c8bf698e45201b839abda83e0320
BluSkreen <jasonduran303@gmail.com>
2022-12-10 23:33:27 -0700

    testing eslint, decided to remove it

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,34 +1,36 @@
-const router = require('express').Router();
-const { Project, User } = require('../models');
-const withAuth = require('../utils/auth');
-
-router.get('/', async (req, res) => {
+const router = require("express").Router();
+// const { Project, User } = require("../models");
+// const withAuth = require("../utils/auth");
+router.get("/", async (req, res) => {
   try {
-    
+    res.status(200);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/search/:id', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-   
+    res.status(200);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-
-router.get('/user/:id', async (req, res) => {
+router.get("/search/:id", async (req, res) => {
   try {
-    
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-
+router.get("/user/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+router.get("/login", (req, res) => {});
 
 module.exports = router;
