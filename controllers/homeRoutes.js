417a03335cb35ecf1f057e606f0657310a31491a
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-12-15 11:50:04 -0700

    got update profile working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -73,6 +73,7 @@ router.get("/profile/:userName", async (req, res) => {
         if (myUserData.dataValues.username === req.params.userName) {
           const user = myUserData.get({ plain: true });
           console.log("Match!");
+          console.log(user);
           res.render("profile", { match: true, user });
         } else {
           // cookies dont match username param
@@ -105,28 +106,27 @@ router.get("/search/:query", async (req, res) => {
     // console.log(query);
     // console.log("---------------")
     const job_tagData = await Job_tag.findAll({
-      include: [Job, Tag]
+      include: [Job, Tag],
     });
 
-    let matchingTags =  await job_tagData.filter((job_tag) => {
+    let matchingTags = await job_tagData.filter((job_tag) => {
       // console.log(job_tag);
       return query === job_tag.tag.dataValues.tag_name;
     });
     if (matchingTags) {
       const jobs = await matchingTags.map((job_tag) => job_tag.job.dataValues);
       console.log(jobs);
-      res.render("search", {jobs});
+      res.render("search", { jobs });
       return;
     } else {
       const jobData = await Job.findAll();
       const jobs = await jobData.map((job) => job.get({ plain: true }));
       // console.log(jobs);
-      res.render("search", {jobs});
+      res.render("search", { jobs });
       return;
     }
-    
-    // console.log(matchingTags);
 
+    // console.log(matchingTags);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -143,7 +143,6 @@ router.get("/search", async (req, res) => {
 
 router.get("/user/:id", async (req, res) => {
   try {
-    
   } catch (err) {
     res.status(500).json(err);
   }
@@ -158,7 +157,7 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/updateProfile", (req, res) => {
-  res.render("updateProfile");
+  res.render("updateProfile", { id: req.session.user_id });
 });
 
 module.exports = router;
--- public/js/updateProfile.js
+++ public/js/updateProfile.js
@@ -1,7 +1,69 @@
-const e = require("express");
+// event.preventDefault();
+// console.log("hello");
+// const username = document.querySelector("#username").value.trim();
+// const first_name = document.querySelector("#first_name").value.trim();
+// const last_name = document.querySelector("#last_name").value.trim();
+// const email = document.querySelector("#email").value.trim();
+// const location = document.querySelector("#location").value.trim();
+// const website = document.querySelector("#website").value.trim();
+// const github = document.querySelector("#github").value.trim();
+// const phone = document.querySelector("#phone").value.trim();
+// const intro = document.querySelector("#intro").value.trim();
+// const aboutMe = document.querySelector("#aboutMe").value.trim();
+// const id = document.getElementById("#username").getAttribute("userId");
+// let inputs = [
+//   username,
+//   first_name,
+//   last_name,
+//   email,
+//   location,
+//   website,
+//   github,
+//   phone,
+//   intro,
+//   aboutMe,
+// ];
+// for (let value in inputs) {
+//   if (value === null) {
+//     value = "";
+//   }
+// }
+// if (
+//   username ||
+//   first_name ||
+//   last_name ||
+//   email ||
+//   location ||
+//   website ||
+//   github ||
+//   phone ||
+//   intro ||
+//   aboutMe
+// ) {
 
-const updateFormHandler = async (event) => {
-  event.preventDefault();
+// const response = await fetch(`/api/user/update/${id}`, {
+//   method: "PUT",
+//   body: JSON.stringify({
+//     first_name,
+//     last_name,
+//     email,
+//     location,
+//     website,
+//     github,
+//     phone,
+//     intro,
+//     aboutMe,
+//   }),
+//   headers: { "Content-Type": "application/json" },
+// });
+// if (response.ok) {
+//   alert("Profile Updated!");
+// } else {
+//   alert("Error: Could not update info");
+// }
+
+document.getElementById("save").addEventListener("click", async (event) => {
+  // event.preventDefault();
 
   const username = document.querySelector("#username").value.trim();
   const first_name = document.querySelector("#first_name").value.trim();
@@ -12,20 +74,50 @@ const updateFormHandler = async (event) => {
   const github = document.querySelector("#github").value.trim();
   const phone = document.querySelector("#phone").value.trim();
   const intro = document.querySelector("#intro").value.trim();
-  const about_me = document.querySelector("#aboutMe").value.trim();
+  const aboutMe = document.querySelector("#aboutMe").value.trim();
+  const id = document.getElementById("username").getAttribute("data-userId");
 
-  if (
-    username ||
-    first_name ||
-    last_name ||
-    email ||
-    location ||
-    website ||
-    github ||
-    phone ||
-    intro ||
-    about_me
-  ) {
-    const response = await fetch("/api/user/")
+  console.log(id);
+  // let inputs = [
+  //   username,
+  //   first_name,
+  //   last_name,
+  //   email,
+  //   location,
+  //   website,
+  //   github,
+  //   phone,
+  //   intro,
+  //   aboutMe,
+  // ];
+
+  // for (let value in inputs) {
+  //   if (value === null) {
+  //     value = "";
+  //   }
+  // }
+
+  const response = await fetch(`/api/user/update/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      first_name,
+      last_name,
+      username,
+      email,
+      location,
+      website,
+      github,
+      phone,
+      intro,
+      aboutMe,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+  const newUserInfo = await response.json();
+  if (response.ok) {
+    console.log(newUserInfo);
+    document.location.replace(`/profile/${newUserInfo.username}`);
+  } else {
+    alert("Error: Could not update info");
   }
-};
+});
