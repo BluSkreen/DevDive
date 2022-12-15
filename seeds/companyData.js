728168910b50c1eb8497f9ea025392d7671de897
edyta0106 <edyta0106@gmail.com>
2022-12-15 12:38:23 -0700

    added job-description partial, finshed updating navbar, added company_link to company model and company seeds, fixed spacing on homepage, and fixed profile image src

--- seeds/companyData.js
+++ seeds/companyData.js
@@ -1,26 +1,32 @@
-const { Company } = require("../models")
+const { Company } = require("../models");
 
 const companyData = [
-    {
-        company_name: "Comcast"
-    },
-    {
-        company_name: "Dish"
-    },
-    {
-        company_name: "Twitter"
-    },
-    {
-        company_name: "Facebook"
-    },
-    {
-        company_name: "Google"
-    },
-    {
-        company_name: "Self-Employed"
-    }
-]
+  {
+    company_name: "Comcast",
+    company_link: "https://jobs.comcast.com/",
+  },
+  {
+    company_name: "Dish",
+    company_link: "https://careers.dish.com/",
+  },
+  {
+    company_name: "Twitter",
+    company_link: "https://careers.twitter.com/",
+  },
+  {
+    company_name: "Facebook",
+    company_link: "https://www.metacareers.com/",
+  },
+  {
+    company_name: "Google",
+    company_link: "https://careers.google.com/",
+  },
+  {
+    company_name: "Self-Employed",
+    company_link: "careers@gmail.com",
+  },
+];
 
-const seedCompanies = () => Company.bulkCreate(companyData)
+const seedCompanies = () => Company.bulkCreate(companyData);
 
-module.exports = seedCompanies
+module.exports = seedCompanies;
